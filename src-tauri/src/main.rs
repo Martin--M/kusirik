8663c7298f6783458f00da317f9fc06d97ba68ff
Martin Mena <martin.mena@outlook.com>
2026-06-24 09:01:29 -0400

    tauri: main: set WEBKIT_DISABLE_DMABUF_RENDERER for WSL
    Without this variable, the rendering didn't really work properly
    when building and running from WSL

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,5 +4,10 @@
 )]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        // Fixes WebKitGTK/WSL2 rendering lockups & blank screen issues
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     iptv_helper::run()
 }
